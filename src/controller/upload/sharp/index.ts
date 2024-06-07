8f571860cdc093bbbc28805b26e5d58d4ab4b1cb
edel <dbfla5036@naver.com>
2024-06-07 16:22:04 +0900

    feat: sharp image

--- src/controller/upload/sharp/index.ts
+++ src/controller/upload/sharp/index.ts
@@ -2,6 +2,18 @@ import { RequestHandler } from 'express';
 import fs from 'fs';
 import sharp from 'sharp';
 
+enum SharpType {
+	resize,
+	rotate,
+	composite,
+}
+
+const typeList = Object.keys(SharpType).filter((key) => isNaN(Number(key)));
+
+export const get: RequestHandler<any, any, any> = async (req, res, next) => {
+	res.json({ success: true, typeList });
+};
+
 export const resize: RequestHandler<any, any, any> = async (req, res, next) => {
 	if (!req.file || !req.file.path) return;
 
@@ -16,10 +28,10 @@ export const resize: RequestHandler<any, any, any> = async (req, res, next) => {
 					if (err) throw err;
 				});
 			});
+		res.json({ filename: `${req.file.filename}` });
 	} catch (err) {
 		console.log(err);
 	}
-	res.json({ filename: `${req.file.filename}` });
 };
 
 export const rotate: RequestHandler<
@@ -44,8 +56,80 @@ export const rotate: RequestHandler<
 					if (err) throw err;
 				});
 			});
+		res.json({ filename: `${req.file.filename}` });
 	} catch (err) {
 		console.log(err);
 	}
-	res.json({ filename: `${req.file.filename}` });
+};
+
+export const sharpType: RequestHandler<{ type: string }, any, any> = async (req, res, next) => {
+	const { type = '' } = req.params;
+	if (!typeList.includes(type)) return next({ status: 404, message: '존재하지 않는 타입입니다.' });
+	if (!req.file || !req.file.path) return next({ status: 404, message: '파일을 첨부해주세요' });
+
+	try {
+		switch (type) {
+			case 'resize':
+				sharp(req.file.path) // 압축할 이미지 경로
+					.resize({ width: 600 }) // 비율을 유지하며 가로 크기 줄이기
+					.withMetadata() // 이미지의 exif데이터 유지
+					.toBuffer((err, buffer) => {
+						if (err) throw err;
+						// 압축된 파일 새로 저장(덮어씌우기)
+						fs.writeFile(req.file!.path, buffer, (err) => {
+							if (err) throw err;
+						});
+					});
+				res.json({ filename: `${req.file.filename}` });
+				break;
+			case 'rotate':
+				const { rotate } = req.body;
+				if (!rotate) return next({ status: 404, message: '회전 정보를 입력해주세요' });
+				sharp(req.file.path) // 압축할 이미지 경로
+					.rotate(Number(rotate)) // rotate
+					.withMetadata() // 이미지의 exif데이터 유지
+					.toBuffer((err, buffer) => {
+						if (err) throw err;
+						// 압축된 파일 새로 저장(덮어씌우기)
+						fs.writeFile(req.file!.path, buffer, (err) => {
+							if (err) throw err;
+						});
+					});
+				res.json({ filename: `${req.file.filename}` });
+				break;
+			case 'composite':
+				const watermark00 = await sharp('uploads/watermark/sy.png').toBuffer();
+				const watermark01 = await sharp('uploads/watermark/sy01.png').toBuffer();
+				const watermark02 = await sharp('uploads/watermark/sy02.png').toBuffer();
+
+				sharp(req.file.path)
+					.composite([
+						{
+							input: watermark00,
+							gravity: 'north',
+						},
+						{
+							input: watermark01,
+							gravity: 'south',
+						},
+						{
+							input: watermark02,
+							gravity: 'east',
+						},
+					])
+					.withMetadata()
+					.toBuffer((err, buffer) => {
+						if (err) throw err;
+						fs.writeFile(req.file!.path, buffer, (err) => {
+							if (err) throw err;
+						});
+					});
+				res.json({ filename: `${req.file.filename}` });
+				break;
+			default:
+				break;
+		}
+	} catch (err) {
+		console.warn(err);
+	}
 };
--- src/router/upload/index.ts
+++ src/router/upload/index.ts
@@ -6,6 +6,6 @@ export const uploadRouter = express.Router();
 
 uploadRouter.post('/', uploader.single('image'), con.upload.uploadController.add);
 
-uploadRouter.post('/sharp', uploader.single('image'), con.upload.uploadController.sharp.resize);
+uploadRouter.get('/sharp', uploader.single('image'), con.upload.uploadController.sharp.get);
 
-uploadRouter.post('/rotate', uploader.single('image'), con.upload.uploadController.sharp.rotate);
+uploadRouter.post('/:type', uploader.single('image'), con.upload.uploadController.sharp.sharpType);
